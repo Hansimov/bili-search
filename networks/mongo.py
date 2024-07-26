b98a77363c108c5eab5623658778999d122ad1ba
Hansimov <591172499@qq.com>
2024-07-27 01:55:57 +0800

    :gem: [Feature] MongoOperator: new get_change_stream with start_at and max_count

--- networks/mongo.py
+++ networks/mongo.py
@@ -1,5 +1,9 @@
+import bson.timestamp
 import pymongo
 
+from datetime import datetime
+from pprint import pformat
+import pymongo.errors
 from tclogger import logger
 
 from configs.envs import MONGO_ENVS
@@ -36,9 +40,46 @@ class MongoOperator:
             cursor = self.db[collection].find()
         return cursor
 
+    def get_change_stream(
+        self,
+        collection,
+        operation_types: list[str] = ["insert", "update"],
+        start_at: int = None,
+        max_count: int = None,
+    ):
+        match_stage = {
+            "$match": {
+                "operationType": {"$in": operation_types},
+            },
+        }
+
+        pipeline = [match_stage]
+
+        if start_at is not None:
+            start_at_operation_time = bson.timestamp.Timestamp(start_at, 0)
+        else:
+            start_at_operation_time = None
+
+        try:
+            change_stream = self.db[collection].watch(
+                pipeline=pipeline, start_at_operation_time=start_at_operation_time
+            )
+        except pymongo.errors.OperationFailure as e:
+            logger.warn(f"× Error: {e}")
+            return
+
+        count = 0
+        for change in change_stream:
+            count += 1
+            if max_count and count > max_count:
+                break
+            logger.mesg(pformat(change, sort_dicts=False, indent=2))
+        change_stream.close()
+
 
 if __name__ == "__main__":
-    syncer = MongoSyncer()
-    syncer.watch()
+    mongo = MongoOperator()
+    start_at_ts = int(datetime.now().timestamp()) - 1 * 60 * 60
+    mongo.get_change_stream("videos", start_at=start_at_ts, max_count=1)
 
-    # python -m elastics.mongo_syncer
+    # python -m networks.mongo
