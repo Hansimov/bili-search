90f83143db44532a316c640699d489ade38a9ef5
Hansimov <591172499@qq.com>
2024-08-24 12:17:22 +0800

    :zap: [Enhance] DateRangeConverter: separate regex to RE and REP (with named groups)

--- converters/date_ranger.py
+++ converters/date_ranger.py
@@ -6,6 +6,7 @@ from tclogger import logger, ts_to_str
 
 
 class DateRangeConverter:
+    # DATE: YMDH, YMD, YM, YYYY, MDH, MD
     RE_MM_SEP = r"[-/\.]+"
     RE_DD_SEP = r"[-/\.]+"
     RE_HH_SEP = r"[\_\.]+"
@@ -16,54 +17,97 @@ class DateRangeConverter:
     RE_SEP_MM = rf"{RE_MM_SEP}{RE_MM}"
     RE_SEP_DD = rf"{RE_DD_SEP}{RE_DD}"
     RE_SEP_HH = rf"{RE_HH_SEP}{RE_HH}"
-    RE_YMDH = rf"(?P<yyyy_mm_dd_hh>(?P<ymdh_year>{RE_YEAR}){RE_MM_SEP}(?P<ymdh_mm>{RE_MM}){RE_DD_SEP}(?P<ymdh_dd>{RE_DD}){RE_HH_SEP}(?P<ymdh_hh>{RE_HH}))"
-    RE_YMD = rf"(?P<yyyy_mm_dd>(?P<ymd_year>{RE_YEAR}){RE_MM_SEP}(?P<ymd_mm>{RE_MM}){RE_DD_SEP}(?P<ymd_dd>{RE_DD}))"
-    RE_YM = rf"(?P<yyyy_mm>(?P<ym_year>{RE_YEAR}){RE_MM_SEP}(?P<ym_mm>{RE_MM}))"
-    RE_YYYY = rf"(?P<yyyy>{RE_YEAR})"
-    RE_MDH = rf"(?P<mm_dd_hh>(?P<mdh_mm>{RE_MM}){RE_DD_SEP}(?P<mdh_dd>{RE_DD}){RE_HH_SEP}(?P<mdh_hh>{RE_HH}))"
-    RE_MD = rf"(?P<mm_dd>(?P<md_mm>{RE_MM}){RE_DD_SEP}(?P<md_dd>{RE_DD}))"
-    RE_RANGE_DATE = (
-        rf"(?P<range_date>{RE_YMDH}|{RE_YMD}|{RE_YM}|{RE_YYYY}|{RE_MDH}|{RE_MD})"
+
+    RE_YMDH = rf"{RE_YEAR}{RE_MM_SEP}{RE_MM}{RE_DD_SEP}{RE_DD}{RE_HH_SEP}{RE_HH}"
+    RE_YMD = rf"{RE_YEAR}{RE_MM_SEP}{RE_MM}{RE_DD_SEP}{RE_DD}"
+    RE_YM = rf"{RE_YEAR}{RE_MM_SEP}{RE_MM}"
+    RE_YYYY = rf"{RE_YEAR}"
+    RE_MDH = rf"{RE_MM}{RE_DD_SEP}{RE_DD}{RE_HH_SEP}{RE_HH}"
+    RE_MD = rf"{RE_MM}{RE_DD_SEP}{RE_DD}"
+    RE_RANGE_DATE = rf"({RE_YMDH}|{RE_YMD}|{RE_YM}|{RE_YYYY}|{RE_MDH}|{RE_MD})"
+
+    REP_YMDH = rf"(?P<yyyy_mm_dd_hh>(?P<ymdh_year>{RE_YEAR}){RE_MM_SEP}(?P<ymdh_mm>{RE_MM}){RE_DD_SEP}(?P<ymdh_dd>{RE_DD}){RE_HH_SEP}(?P<ymdh_hh>{RE_HH}))"
+    REP_YMD = rf"(?P<yyyy_mm_dd>(?P<ymd_year>{RE_YEAR}){RE_MM_SEP}(?P<ymd_mm>{RE_MM}){RE_DD_SEP}(?P<ymd_dd>{RE_DD}))"
+    REP_YM = rf"(?P<yyyy_mm>(?P<ym_year>{RE_YEAR}){RE_MM_SEP}(?P<ym_mm>{RE_MM}))"
+    REP_YYYY = rf"(?P<yyyy>{RE_YEAR})"
+    REP_MDH = rf"(?P<mm_dd_hh>(?P<mdh_mm>{RE_MM}){RE_DD_SEP}(?P<mdh_dd>{RE_DD}){RE_HH_SEP}(?P<mdh_hh>{RE_HH}))"
+    REP_MD = rf"(?P<mm_dd>(?P<md_mm>{RE_MM}){RE_DD_SEP}(?P<md_dd>{RE_DD}))"
+    REP_RANGE_DATE = (
+        rf"(?P<range_date>{REP_YMDH}|{REP_YMD}|{REP_YM}|{REP_YYYY}|{REP_MDH}|{REP_MD})"
     )
 
+    # THIS/PAST/LAST + YEAR/MONTH/WEEK/DAY/HOUR
     RE_CH_SEP = r"[\_\.\s\-]+"
-    RE_THIS_YEAR = rf"(?P<this_year>本年|今年|this{RE_CH_SEP}year)"
-    RE_LAST_YEAR = rf"(?P<last_year>上年|去年|last{RE_CH_SEP}year)"
-    RE_PAST_YEAR = rf"(?P<past_year>过去一年|past{RE_CH_SEP}year)"
-    RE_THIS_MONTH = rf"(?P<this_month>本月|this{RE_CH_SEP}month)"
-    RE_LAST_MONTH = rf"(?P<last_month>上个?月|last{RE_CH_SEP}month)"
-    RE_PAST_MONTH = rf"(?P<past_month>过去一个?月|past{RE_CH_SEP}month)"
-    RE_THIS_WEEK = rf"(?P<this_week>本周|this{RE_CH_SEP}week)"
-    RE_LAST_WEEK = rf"(?P<last_week>上周|last{RE_CH_SEP}week)"
-    RE_PAST_WEEK = rf"(?P<past_week>过去一周|past{RE_CH_SEP}week)"
-    RE_THIS_DAY = rf"(?P<this_day>本日|今日|今天|this{RE_CH_SEP}day|today)"
-    RE_LAST_DAY = rf"(?P<last_day>上日|昨日|昨天|last{RE_CH_SEP}day|yesterday)"
-    RE_PAST_DAY = rf"(?P<past_day>过去一天|past{RE_CH_SEP}day)"
-    RE_THIS_HOUR = rf"(?P<this_hour>本时|当前小时|this{RE_CH_SEP}hour)"
-    RE_LAST_HOUR = rf"(?P<last_hour>上时|上个小时|last{RE_CH_SEP}hour)"
-    RE_PAST_HOUR = rf"(?P<past_hour>过去一个小时|past{RE_CH_SEP}hour)"
-    RE_RANGE_THIS = rf"(?P<range_this>{RE_THIS_YEAR}|{RE_THIS_MONTH}|{RE_THIS_WEEK}|{RE_THIS_DAY}|{RE_THIS_HOUR})"
-    RE_RANGE_LAST = rf"(?P<range_last>{RE_LAST_YEAR}|{RE_LAST_MONTH}|{RE_LAST_WEEK}|{RE_LAST_DAY}|{RE_LAST_HOUR})"
-    RE_RANGE_PAST = rf"(?P<range_past>{RE_PAST_YEAR}|{RE_PAST_MONTH}|{RE_PAST_WEEK}|{RE_PAST_DAY}|{RE_PAST_HOUR})"
-
-    RE_DU_SEP = r"[\s]*"
-    RE_YEAR_UNIT = r"(?P<year_unit>年|years?|yr|y)"
-    RE_MONTH_UNIT = r"(?P<month_unit>个?月|months?|mon)"
-    RE_WEEK_UNIT = r"(?P<week_unit>周|weeks?|wk|w)"
-    RE_DAY_UNIT = r"(?P<day_unit>日|天|days?|d)"
-    RE_HOUR_UNIT = r"(?P<hour_unit>个?小时|hours?|hr|h)"
-    # RE_MINUTE_UNIT = r"(分钟?|minutes?|min)"
-    # RE_SECOND_UNIT = r"(秒钟?|seconds?|sec|s)"
-    RE_N_YEAR = rf"(?P<n_years>(?P<year_n>\d+){RE_DU_SEP}{RE_YEAR_UNIT})"
-    RE_N_MONTH = rf"(?P<n_months>(?P<month_n>\d+){RE_DU_SEP}{RE_MONTH_UNIT})"
-    RE_N_WEEK = rf"(?P<n_weeks>(?P<week_n>\d+){RE_DU_SEP}{RE_WEEK_UNIT})"
-    RE_N_DAY = rf"(?P<n_days>(?P<day_n>\d+){RE_DU_SEP}{RE_DAY_UNIT})"
-    RE_N_HOUR = rf"(?P<n_hours>(?P<hour_n>\d+){RE_DU_SEP}{RE_HOUR_UNIT})"
-    # RE_N_MINUTE = rf"(?P<n_minutes>(?P<minute_n>\d+)\s*{RE_MINUTE_UNIT})"
-    # RE_N_SECOND = rf"(?P<n_seconds>(?P<second_n>\d+)\s*{RE_SECOND_UNIT})"
-    RE_RANGE_DIST = (
-        rf"(?P<range_dist>{RE_N_YEAR}|{RE_N_MONTH}|{RE_N_WEEK}|{RE_N_DAY}|{RE_N_HOUR})"
+    RE_THIS_YEAR = rf"(本年|今年|this{RE_CH_SEP}year)"
+    RE_LAST_YEAR = rf"(上年|去年|last{RE_CH_SEP}year)"
+    RE_PAST_YEAR = rf"(过去一年|past{RE_CH_SEP}year)"
+    RE_THIS_MONTH = rf"(本月|this{RE_CH_SEP}month)"
+    RE_LAST_MONTH = rf"(上个?月|last{RE_CH_SEP}month)"
+    RE_PAST_MONTH = rf"(过去一个?月|past{RE_CH_SEP}month)"
+    RE_THIS_WEEK = rf"(本周|this{RE_CH_SEP}week)"
+    RE_LAST_WEEK = rf"(上周|last{RE_CH_SEP}week)"
+    RE_PAST_WEEK = rf"(过去一周|past{RE_CH_SEP}week)"
+    RE_THIS_DAY = rf"(本日|今日|今天|this{RE_CH_SEP}day|today)"
+    RE_LAST_DAY = rf"(上日|昨日|昨天|last{RE_CH_SEP}day|yesterday)"
+    RE_PAST_DAY = rf"(过去一天|past{RE_CH_SEP}day)"
+    RE_THIS_HOUR = rf"(本时|当前小时|this{RE_CH_SEP}hour)"
+    RE_LAST_HOUR = rf"(上时|上个小时|last{RE_CH_SEP}hour)"
+    RE_PAST_HOUR = rf"(过去一个小时|past{RE_CH_SEP}hour)"
+    RE_RANGE_THIS = (
+        rf"({RE_THIS_YEAR}|{RE_THIS_MONTH}|{RE_THIS_WEEK}|{RE_THIS_DAY}|{RE_THIS_HOUR})"
     )
+    RE_RANGE_LAST = (
+        rf"({RE_LAST_YEAR}|{RE_LAST_MONTH}|{RE_LAST_WEEK}|{RE_LAST_DAY}|{RE_LAST_HOUR})"
+    )
+    RE_RANGE_PAST = (
+        rf"({RE_PAST_YEAR}|{RE_PAST_MONTH}|{RE_PAST_WEEK}|{RE_PAST_DAY}|{RE_PAST_HOUR})"
+    )
+    RE_RANGE_RECENT = rf"({RE_RANGE_THIS}|{RE_RANGE_LAST}|{RE_RANGE_PAST})"
+
+    REP_THIS_YEAR = rf"(?P<this_year>{RE_THIS_YEAR})"
+    REP_LAST_YEAR = rf"(?P<last_year>{RE_LAST_YEAR})"
+    REP_PAST_YEAR = rf"(?P<past_year>{RE_PAST_YEAR})"
+    REP_THIS_MONTH = rf"(?P<this_month>{RE_THIS_MONTH})"
+    REP_LAST_MONTH = rf"(?P<last_month>{RE_LAST_MONTH})"
+    REP_PAST_MONTH = rf"(?P<past_month>{RE_PAST_MONTH})"
+    REP_THIS_WEEK = rf"(?P<this_week>{RE_THIS_WEEK})"
+    REP_LAST_WEEK = rf"(?P<last_week>{RE_LAST_WEEK})"
+    REP_PAST_WEEK = rf"(?P<past_week>{RE_PAST_WEEK})"
+    REP_THIS_DAY = rf"(?P<this_day>{RE_THIS_DAY})"
+    REP_LAST_DAY = rf"(?P<last_day>{RE_LAST_DAY})"
+    REP_PAST_DAY = rf"(?P<past_day>{RE_PAST_DAY})"
+    REP_THIS_HOUR = rf"(?P<this_hour>{RE_THIS_HOUR})"
+    REP_LAST_HOUR = rf"(?P<last_hour>{RE_LAST_HOUR})"
+    REP_PAST_HOUR = rf"(?P<past_hour>{RE_PAST_HOUR})"
+    REP_RANGE_THIS = rf"(?P<range_this>{REP_THIS_YEAR}|{REP_THIS_MONTH}|{REP_THIS_WEEK}|{REP_THIS_DAY}|{REP_THIS_HOUR})"
+    REP_RANGE_LAST = rf"(?P<range_last>{REP_LAST_YEAR}|{REP_LAST_MONTH}|{REP_LAST_WEEK}|{REP_LAST_DAY}|{REP_LAST_HOUR})"
+    REP_RANGE_PAST = rf"(?P<range_past>{REP_PAST_YEAR}|{REP_PAST_MONTH}|{REP_PAST_WEEK}|{REP_PAST_DAY}|{REP_PAST_HOUR})"
+    REP_RANGE_RECENT = (
+        rf"(?P<range_recent>{REP_RANGE_THIS}|{REP_RANGE_LAST}|{REP_RANGE_PAST})"
+    )
+
+    # N + YEAR/MONTH/WEEK/DAY/HOUR
+    RE_DU_SEP = r"[\s]*"
+    RE_YEAR_UNIT = r"(年|years?|yr|y)"
+    RE_MONTH_UNIT = r"(个?月|months?|mon)"
+    RE_WEEK_UNIT = r"(周|weeks?|wk|w)"
+    RE_DAY_UNIT = r"(日|天|days?|d)"
+    RE_HOUR_UNIT = r"(个?小时|hours?|hr|h)"
+    RE_N_YEAR = rf"\d+{RE_DU_SEP}{RE_YEAR_UNIT}"
+    RE_N_MONTH = rf"\d+{RE_DU_SEP}{RE_MONTH_UNIT}"
+    RE_N_WEEK = rf"\d+{RE_DU_SEP}{RE_WEEK_UNIT}"
+    RE_N_DAY = rf"\d+{RE_DU_SEP}{RE_DAY_UNIT}"
+    RE_N_HOUR = rf"\d+{RE_DU_SEP}{RE_HOUR_UNIT}"
+    RE_RANGE_DIST = rf"({RE_N_YEAR}|{RE_N_MONTH}|{RE_N_WEEK}|{RE_N_DAY}|{RE_N_HOUR})"
+
+    REP_N_YEAR = rf"(?P<n_years>(?P<year_n>\d+){RE_DU_SEP}{RE_YEAR_UNIT})"
+    REP_N_MONTH = rf"(?P<n_months>(?P<month_n>\d+){RE_DU_SEP}{RE_MONTH_UNIT})"
+    REP_N_WEEK = rf"(?P<n_weeks>(?P<week_n>\d+){RE_DU_SEP}{RE_WEEK_UNIT})"
+    REP_N_DAY = rf"(?P<n_days>(?P<day_n>\d+){RE_DU_SEP}{RE_DAY_UNIT})"
+    REP_N_HOUR = rf"(?P<n_hours>(?P<hour_n>\d+){RE_DU_SEP}{RE_HOUR_UNIT})"
+    REP_RANGE_DIST = rf"(?P<range_dist>{REP_N_YEAR}|{REP_N_MONTH}|{REP_N_WEEK}|{REP_N_DAY}|{REP_N_HOUR})"
+
+    RE_DATE_ALL = rf"{RE_RANGE_DATE}|{RE_RANGE_RECENT}|{RE_RANGE_DIST}"
 
     def get_date_ts_range(self, date_str: str) -> tuple[int, int]:
         """Get the timestamp range of the date.
@@ -134,23 +178,23 @@ class DateRangeConverter:
                 -   to: [DATE_OF_FIVE_HOURS_AGO]
         """
 
-        if re.match(self.RE_RANGE_DATE, date_str):
+        if re.match(self.REP_RANGE_DATE, date_str):
             return self.get_date_ts_range_of_date(date_str)
-        elif re.match(self.RE_RANGE_THIS, date_str):
+        elif re.match(self.REP_RANGE_THIS, date_str):
             return self.get_date_ts_range_of_this(date_str)
-        elif re.match(self.RE_RANGE_LAST, date_str):
+        elif re.match(self.REP_RANGE_LAST, date_str):
             return self.get_date_ts_range_of_last(date_str)
-        elif re.match(self.RE_RANGE_DIST, date_str):
+        elif re.match(self.REP_RANGE_DIST, date_str):
             return self.get_date_ts_range_of_dist(date_str)
-        elif re.match(self.RE_RANGE_PAST, date_str):
+        elif re.match(self.REP_RANGE_PAST, date_str):
             return self.get_date_ts_range_of_past(date_str)
         else:
             logger.warn(f"× No matched range for date_str: {date_str}")
-            return None
+            return 0, 0
 
     def get_date_ts_range_of_date(self, date_str: str) -> tuple[int, int]:
         now = datetime.now()
-        match = re.match(self.RE_RANGE_DATE, date_str)
+        match = re.match(self.REP_RANGE_DATE, date_str)
         if match:
             if match.group("yyyy_mm_dd_hh"):
                 year = int(match.group("ymdh_year"))
@@ -197,12 +241,14 @@ class DateRangeConverter:
                 return int(start_dt.timestamp()), int(end_dt.timestamp())
             else:
                 return 0, 0
+        else:
+            logger.warn(f"× No match for type <range_date>: {date_str}")
 
         return 0, 0
 
     def get_date_ts_range_of_this(self, date_str: str) -> tuple[int, int]:
         now = datetime.now()
-        match = re.match(self.RE_RANGE_THIS, date_str)
+        match = re.match(self.REP_RANGE_THIS, date_str)
         if match:
             if match.group("this_year"):
                 start_dt = datetime(now.year, 1, 1)
@@ -223,12 +269,14 @@ class DateRangeConverter:
                 return int(start_dt.timestamp()), int(now.timestamp())
             else:
                 return 0, 0
+        else:
+            logger.warn(f"× No match for type <range_this>: {date_str}")
 
         return 0, 0
 
     def get_date_ts_range_of_last(self, date_str: str) -> tuple[int, int]:
         now = datetime.now()
-        match = re.match(self.RE_RANGE_LAST, date_str)
+        match = re.match(self.REP_RANGE_LAST, date_str)
         if match:
             if match.group("last_year"):
                 start_dt = datetime(now.year - 1, 1, 1)
@@ -262,13 +310,15 @@ class DateRangeConverter:
                 return int(start_dt.timestamp()), int(end_dt.timestamp())
             else:
                 return 0, 0
+        else:
+            logger.warn(f"× No match for type <range_last>: {date_str}")
 
         return 0, 0
 
     def get_date_ts_range_of_dist(self, date_str: str) -> tuple[int, int]:
         now = datetime.now()
 
-        match = re.match(self.RE_RANGE_DIST, date_str)
+        match = re.match(self.REP_RANGE_DIST, date_str)
         if match:
             if match.group("n_years"):
                 n = int(match.group("year_n"))
@@ -299,11 +349,13 @@ class DateRangeConverter:
                 return int(start_dt.timestamp()), int(now.timestamp())
             else:
                 return 0, 0
+        else:
+            logger.warn(f"× No match for type <range_dist>: {date_str}")
 
         return 0, 0
 
     def get_date_ts_range_of_past(self, date_str: str) -> tuple[int, int]:
-        match = re.match(self.RE_RANGE_PAST, date_str)
+        match = re.match(self.REP_RANGE_PAST, date_str)
         if match:
             if match.group("past_year"):
                 return self.get_date_ts_range_of_dist("1 year")
@@ -324,26 +376,26 @@ class DateRangeConverter:
 
 if __name__ == "__main__":
     date_strs = [
-        # "2014",
-        # "2014-02",
-        # "2014/08/12",
-        # "08/12",
-        # "2014-08-12.12",
-        # "this_year",
-        # "this_month",
-        # "this_week",
-        # "this_day",
-        # "this_hour",
-        # "last_year",
-        # "last_month",
-        # "last_week",
-        # "last_day",
-        # "last_hour",
-        # "1 year",
-        # "2 months",
-        # "3 weeks",
-        # "4 days",
-        # "5 hours",
+        "2014",
+        "2014-02",
+        "2014/08/12",
+        "08/12",
+        "2014-08-12.12",
+        "this_year",
+        "this_month",
+        "this_week",
+        "this_day",
+        "this_hour",
+        "last_year",
+        "last_month",
+        "last_week",
+        "last_day",
+        "last_hour",
+        "1 year",
+        "2 months",
+        "3 weeks",
+        "4 days",
+        "5 hours",
         "past year",
         "past.month",
         "past-week",
