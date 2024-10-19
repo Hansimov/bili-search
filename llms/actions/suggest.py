49523d6285fba529d8dab126dac4a95250b17bbc
Hansimov <591172499@qq.com>
2024-10-19 10:31:21 +0800

    :zap: [Enhance] rename QuerySuggester to VideoSuggester, and add param of limit

--- llms/actions/suggest.py
+++ llms/actions/suggest.py
@@ -5,15 +5,17 @@ from elastics.videos.searcher import VideoSearcher
 from configs.envs import SEARCH_APP_ENVS
 
 
-class QuerySuggester:
+class VideoSuggester:
     def __init__(
         self,
         mode: Literal["prod", "dev"] = "dev",
         source_fields: list[str] = ["title", "bvid", "owner"],
+        limit: int = 10,
     ):
         index_name = SEARCH_APP_ENVS["bili_videos_index"][mode]
         self.searcher = VideoSearcher(index_name, elastic_verbose=False)
         self.source_fields = source_fields
+        self.limit = limit
 
     def reduce_hits_by_source_fields(
         self, hits: list[dict], source_fields: list[str] = []
@@ -23,10 +25,13 @@ class QuerySuggester:
         ]
         return new_hits
 
-    def suggest(self, query: str, source_fields: list[str] = []) -> dict:
+    def suggest(
+        self, query: str, source_fields: list[str] = [], limit: int = 10
+    ) -> dict:
         source_fields = source_fields or self.source_fields
+        limit = limit or self.limit
         res = self.searcher.multi_level_suggest(
-            query, source_fields=source_fields, limit=5
+            query, source_fields=source_fields, limit=limit
         )
         hits = res.get("hits", [])
         hits = self.reduce_hits_by_source_fields(hits, source_fields=source_fields)
@@ -37,8 +42,8 @@ class QuerySuggester:
 if __name__ == "__main__":
     query = "红警 月亮3"
     logger.note(f"> Query: [{logstr.mesg(query)}]")
-    suggester = QuerySuggester()
+    suggester = VideoSuggester()
     hits = suggester.suggest(query)
     logger.success(dict_to_str(hits, add_quotes=True))
 
-    # python -m agents.suggest
+    # python -m llms.actions.suggest
