8a0437f5773d4d9e4e9e4394ba2a60c4d80f94a1
Hansimov <591172499@qq.com>
2024-10-27 16:48:35 +0800

    :zap: [Enhance] add construct_chat_resp_dict with MESSAGE_TYPE of RECV and SEND in Messager, and wrap ws.send_text in chat_delta_func in Router

--- llms/ws/route.py
+++ llms/ws/route.py
@@ -2,21 +2,33 @@ import asyncio
 import json
 
 from fastapi import WebSocket, WebSocketDisconnect
-from tclogger import TCLogger
 from functools import partial
+from tclogger import TCLogger
+from typing import Literal
 
 from llms.agents.copilot import CopilotAgent
 
 
 logger = TCLogger()
 
+RECV_MESSAGE_TYPE = Literal["chat"]
+SEND_MESSAGE_TYPE = Literal["chat"]
+
 
 class WebsocketMessager:
-    def parse(self, message: str):
+    def parse(self, message: RECV_MESSAGE_TYPE):
         data = json.loads(message)
         message_type = data.get("type", None)
         message_info = data.get("info", None)
         return message_type, message_info
+
+    def construct_chat_resp_dict(
+        self, role: str, content: str, msg_type: SEND_MESSAGE_TYPE
+    ):
+        return {
+            "type": msg_type,
+            "info": {"role": role, "content": content},
+        }
 
 
 class WebsocketRouter:
@@ -24,6 +36,10 @@ class WebsocketRouter:
         self.ws = ws
         self.messager = WebsocketMessager()
         self.verbose = verbose
+
+    async def chat_delta_func(self, role: str, content: str):
+        msg = self.messager.construct_chat_resp_dict(role, content, msg_type="chat")
+        await self.ws.send_text(json.dumps(msg))
 
     async def run(self):
         await self.ws.accept()
@@ -33,7 +49,7 @@ class WebsocketRouter:
                 msg_type, msg_info = self.messager.parse(msg_str)
                 if msg_type == "chat":
                     copilot = CopilotAgent(
-                        delta_func=self.ws.send_text, verbose_chat=False
+                        delta_func=self.chat_delta_func, verbose_chat=False
                     )
                     await asyncio.to_thread(copilot.chat, msg_info)
             except WebSocketDisconnect:
