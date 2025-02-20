fb9a51cad7f98a6b7ed0d19153c162e923c18cd0
Hansimov <591172499@qq.com>
2025-02-20 09:22:01 +0800

    :zap: [Enhance] dsl.lark: add syntax of word_expr

--- converters/syntax/test.py
+++ converters/syntax/test.py
@@ -15,7 +15,7 @@ user_queries = [
     "u=影视飓风",
     "u==咬人猫=",
     "用户!==飓多多StormCrew",
-    "@-LKs-,红警HBK08，",
+    "@！-LKs-,红警HBK08，",
 ]
 uid_queries = [
     "uid>1234",
@@ -33,11 +33,20 @@ region_queries = [
     "rid=1,24,153",
     "r- =影视,动画,153",
 ]
+word_queries = [
+    "k=你好",
+    'k="世界,你好"',
+    "k!=[你好，世界]",
+    "k-=[你好,世界]",
+    'k=["你好,世界","再见，故乡"]',
+    "k=3-0",
+]
 
 queries = [
     # *date_queries,
     # *user_queries,
     # *uid_queries,
     # *stat_queries,
-    *region_queries,
+    # *region_queries,
+    *word_queries,
 ]
