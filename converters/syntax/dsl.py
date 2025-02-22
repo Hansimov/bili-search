71c115204443ab9e182c44042a1a9435466a8d84
Hansimov <591172499@qq.com>
2025-02-22 14:05:08 +0800

    :zap: [Enhance] separate DslLarkParser to independent module from DslToElasticConverter

---
+++ converters/syntax/dsl.py
@@ -0,0 +1,65 @@
+from lark import Lark, Token, Tree
+from pathlib import Path
+from tclogger import logger, logstr
+from typing import Union, Literal
+
+
+class DslLarkParser:
+    def __init__(self, verbose: bool = False):
+        self.dsl_lark = Path(__file__).parent / "dsl.lark"
+        self.verbose = verbose
+        self.init_parser()
+
+    def init_parser(self):
+        with open(self.dsl_lark, "r") as rf:
+            syntax = rf.read()
+        self.parser = Lark(syntax, parser="earley")
+
+    def parse(self, expr: str) -> Tree:
+        try:
+            return self.parser.parse(expr)
+        except Exception as e:
+            logger.warn(f"× {expr}")
+            raise e
+
+    def log_node(
+        self,
+        node: Union[Tree, Token],
+        node_type: Literal["tree", "token", "unknown"] = "unknown",
+        level: int = 0,
+    ):
+        indent_str = " " * 2 * level
+        if node_type == "tree":
+            if level > 0:
+                indent_str += "- "
+            node_str = f"{indent_str}{logstr.file(node.data)}:"
+            logger.note(node_str, verbose=self.verbose)
+        elif node_type == "token":
+            node_str = (
+                f'{indent_str}{logstr.file(node.type)}: "{logstr.line(node.value)}"'
+            )
+            logger.mesg(node_str, verbose=self.verbose)
+        else:
+            node_str = f"{indent_str}Unknown: None"
+            logger.warn(node_str, verbose=self.verbose)
+        self.tree_str += f"{node_str}\n"
+
+    def traverse_node(
+        self,
+        node: Union[Tree, Token],
+        level: int = 0,
+        node_func: callable = log_node,
+    ):
+        if isinstance(node, Tree):
+            node_func(node, node_type="tree", level=level)
+            for child in node.children:
+                self.traverse_node(child, level=level + 1, node_func=node_func)
+        elif isinstance(node, Token):
+            node_func(node, node_type="token", level=level)
+        else:
+            node_func(node, node_type="unknown", level=level)
+
+    def str(self, tree: Tree):
+        self.tree_str = ""
+        self.traverse_node(tree, level=0, node_func=self.log_node)
+        return self.tree_str
--- converters/syntax/elastic.py
+++ converters/syntax/elastic.py
@@ -1,78 +1,27 @@
-import yaml
+from tclogger import logger
 
-from lark import Lark, Token, Tree
-from pathlib import Path
-from tclogger import logger, dict_to_str
-from typing import Union, Literal
-
-from converters.syntax.node import DslNode, DslTreeBuilder
+from converters.syntax.dsl import DslLarkParser
+from converters.syntax.node import DslTreeBuilder, DslTreeExprGrouper
 
 
 class DslToElasticConverter:
     def __init__(self, verbose: bool = False):
-        self.dsl_lark = Path(__file__).parent / "dsl.lark"
         self.verbose = verbose
-        self.init_parser()
-
-    def init_parser(self):
-        with open(self.dsl_lark, "r") as rf:
-            syntax = rf.read()
-        self.parser = Lark(syntax, parser="earley")
-
-    def parse_as_tree(self, expr: str) -> Tree:
-        try:
-            tree = self.parser.parse(expr)
-            logger.success(f"✓ {expr}", verbose=self.verbose)
-            return tree
-        except Exception as e:
-            logger.warn(f"× {expr}", verbose=self.verbose)
-            if self.verbose:
-                raise e
-            return None
-
-    def log_node(
-        self,
-        node: Union[Tree, Token],
-        node_type: Literal["tree", "token", "unknown"] = "unknown",
-        level: int = 0,
-    ):
-        indent_str = " " * 2 * level
-        if node_type == "tree":
-            if level > 0:
-                indent_str += "- "
-            node_str = f"{indent_str}{node.data}:"
-            logger.note(node_str, verbose=self.verbose)
-        elif node_type == "token":
-            node_str = f'{indent_str}{node.type}: "{node.value}"'
-            logger.mesg(node_str, verbose=self.verbose)
-        else:
-            node_str = f"{indent_str}Unknown: None"
-            logger.warn(node_str, verbose=self.verbose)
-        self.tree_str += f"{node_str}\n"
-
-    def traverse_node(
-        self,
-        node: Union[Tree, Token],
-        level: int = 0,
-        node_func: callable = log_node,
-    ):
-        if isinstance(node, Tree):
-            node_func(node, node_type="tree", level=level)
-            for child in node.children:
-                self.traverse_node(child, level=level + 1, node_func=node_func)
-        elif isinstance(node, Token):
-            node_func(node, node_type="token", level=level)
-        else:
-            node_func(node, node_type="unknown", level=level)
 
     def construct(self, expr: str) -> dict:
         self.tree_dict = {}
-        self.tree_str = ""
-        tree = self.parse_as_tree(expr)
-        self.traverse_node(tree, node_func=self.log_node)
-        dsl_tree = DslTreeBuilder().build_dsl_tree_from_lark_tree(tree)
-        logger.mesg(dsl_tree.str(), verbose=dsl_tree)
-        # self.tree_dict = yaml.safe_load(self.tree_str)
+
+        parser = DslLarkParser()
+        lark_tree = parser.parse(expr)
+        logger.mesg(parser.str(lark_tree), verbose=lark_tree)
+
+        builder = DslTreeBuilder()
+        dsl_tree = builder.build_dsl_tree_from_lark_tree(lark_tree)
+        logger.mesg(str(dsl_tree), verbose=dsl_tree)
+
+        grouper = DslTreeExprGrouper()
+        expr_tree = grouper.group_dsl_tree_to_expr_tree(dsl_tree)
+        logger.mesg(str(expr_tree), verbose=expr_tree)
         return self.tree_dict
 
 
@@ -81,7 +30,7 @@ def test_dsl_to_elastic_converter():
 
     converter = DslToElasticConverter(verbose=True)
 
-    for query in queries[:1]:
+    for query in queries[-1:]:
         tree_dict = converter.construct(query)
         # logger.mesg(dict_to_str(tree_dict))
 
