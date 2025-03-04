fb4f0b62343cbcdf63e920fa5b5a2711a82f3e08
Hansimov <591172499@qq.com>
2025-03-04 14:32:55 +0800

    :zap: [Enhance] DslExprToElasticConverter: new word_cluster_to_elastic_dict, and init converters instances for atom_node_to_elastic_dict

--- converters/dsl/elastic.py
+++ converters/dsl/elastic.py
@@ -1,11 +1,13 @@
 from tclogger import logger, dict_to_str
 
+from converters.dsl.constants import BOOL_OPS, ATOMS
 from converters.dsl.parse import DslLarkParser
 from converters.dsl.node import DslExprNode, DslTreeBuilder, DslTreeExprGrouper
+from converters.dsl.node import DslExprTreeFlatter
 from converters.dsl.fields.date import DateExprElasticConverter
 from converters.dsl.fields.user import UserExprElasticConverter
 from converters.dsl.fields.word import WordExprElasticConverter
-from converters.dsl.constants import BOOL_OPS, ATOMS
+from converters.dsl.fields.bool import BoolElasticReducer
 
 
 class DslExprToElasticConverter(DslTreeExprGrouper):
@@ -13,15 +15,47 @@ class DslExprToElasticConverter(DslTreeExprGrouper):
         self.parser = DslLarkParser()
         self.builder = DslTreeBuilder()
         self.grouper = DslTreeExprGrouper()
+        self.flatter = DslExprTreeFlatter()
+        self.reducer = BoolElasticReducer()
+        self.date_converter = DateExprElasticConverter()
+        self.user_converter = UserExprElasticConverter()
+        self.word_converter = WordExprElasticConverter()
+
+    def atom_node_to_elastic_dict(self, node: DslExprNode) -> dict:
+        expr_node = node.find_child_with_key(ATOMS)
+        if expr_node.is_key("date_expr"):
+            return self.date_converter.convert(node)
+        elif expr_node.is_key("user_expr"):
+            return self.user_converter.convert(node)
+        elif expr_node.is_key("word_expr"):
+            return self.word_converter.convert(node)
+        else:
+            logger.warn(f"× Unknown atom_node: <{expr_node.key}>")
+            return {}
+
+    def word_cluster_to_elastic_dict(self, node: DslExprNode) -> dict:
+        word_expr_nodes = node.find_all_childs_with_key("word_expr")
+        word_expr_dicts = [
+            self.word_converter.convert(word_expr_node)
+            for word_expr_node in word_expr_nodes
+        ]
+        elatic_dict = self.reducer.reduce_bool_clauses(word_expr_dicts)
+        return elatic_dict
 
     def bool_node_to_elastic_dict(self, node: DslExprNode) -> dict:
         if node.is_key("pa"):
             return self.node_to_elastic_dict(node.children[0])
         elif node.is_key(["and", "co"]):
-            musts = []
-            for child in node.children:
-                musts.append(self.node_to_elastic_dict(child))
-            return {"bool": {"must": musts}}
+            if (
+                node.is_all_bool_childs_are_co_and()
+                and node.is_all_atom_childs_are_word_expr()
+            ):
+                return self.word_cluster_to_elastic_dict(node)
+            else:
+                musts = []
+                for child in node.children:
+                    musts.append(self.node_to_elastic_dict(child))
+                return {"bool": {"must": musts}}
         elif node.is_key("or"):
             shoulds = []
             for child in node.children:
@@ -31,22 +65,11 @@ class DslExprToElasticConverter(DslTreeExprGrouper):
             logger.warn(f"× Unknown bool_node: <{node.key}>")
             return {}
 
-    def atom_node_to_elastic_dict(self, node: DslExprNode) -> dict:
-        expr_node = node.find_child_with_key(ATOMS)
-        if expr_node.is_key("date_expr"):
-            return DateExprElasticConverter().convert(node)
-        elif expr_node.is_key("user_expr"):
-            return UserExprElasticConverter().convert(node)
-        elif expr_node.is_key("word_expr"):
-            return WordExprElasticConverter().convert(node)
-        else:
-            logger.warn(f"× Unknown atom_node: <{expr_node.key}>")
-            return {}
-
     def node_to_elastic_dict(self, node: DslExprNode) -> dict:
         expr_node = node.find_child_with_key([*BOOL_OPS, "atom"])
         if expr_node.is_key(BOOL_OPS):
-            return self.bool_node_to_elastic_dict(expr_node)
+            res = self.bool_node_to_elastic_dict(expr_node)
+            return self.reducer.reduce(res)
         elif expr_node.is_key("atom"):
             return self.atom_node_to_elastic_dict(expr_node)
         else:
@@ -59,6 +82,8 @@ class DslExprToElasticConverter(DslTreeExprGrouper):
         dsl_tree = self.builder.build_dsl_tree_from_lark_tree(lark_tree)
         # logger.mesg(str(dsl_tree), verbose=dsl_tree)
         expr_tree = self.grouper.group_dsl_tree_to_expr_tree(dsl_tree)
+        # logger.mesg(str(expr_tree), verbose=expr_tree)
+        expr_tree = self.flatter.flatten(expr_tree)
         logger.mesg(str(expr_tree), verbose=expr_tree)
         elastic_dict = {"query": self.node_to_elastic_dict(expr_tree)}
         return elastic_dict
