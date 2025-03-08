4ed21a7af0b00ede32185249bbc577a6f5f81c5e
Hansimov <591172499@qq.com>
2025-03-08 14:29:32 +0800

    :recycle: [Refactor] VideoHitsParser: moduralize split_query_to_keywords_and_filters, get_merged_fields, and get_merged_and_segged_highlights

--- elastics/videos/hits.py
+++ elastics/videos/hits.py
@@ -1,40 +1,89 @@
-from typing import Literal
 from tclogger import logger
+from typing import Union
 
 from elastics.structure import get_es_source_val
 from converters.highlight.merge import HighlightMerger
 from converters.highlight.count import HighlightsCounter
 from converters.highlight.pinyin import PinyinHighlighter
 from converters.query.filter import QueryFilterExtractor
+from converters.dsl.elastic import DslExprKeywordsFilterSplitter
 from elastics.videos.constants import MATCH_TYPE, MATCH_OPERATOR
 from elastics.videos.constants import SEARCH_MATCH_TYPE, SEARCH_MATCH_OPERATOR
 from elastics.videos.constants import SEARCH_REQUEST_TYPE, SEARCH_REQUEST_TYPE_DEFAULT
 
 
-class VideoHitsParser:
+class VideoHitsParserV1:
     def __init__(self):
         self.pinyin_highlighter = PinyinHighlighter()
         self.highlights_counter = HighlightsCounter()
-        self.split_query = QueryFilterExtractor().split_keyword_and_filter_expr
+        self.highlight_merger = HighlightMerger()
+
+    def split_query_to_keywords_and_filters(self, query: str) -> dict:
+        return QueryFilterExtractor().split_keyword_and_filter_expr(query)
 
     def get_pinyin_highlights(
-        self, query: str, match_fields: list[str], _source: dict
+        self, keywords: Union[str, list[str]], match_fields: list[str], source: dict
     ) -> dict:
-        if not query:
+        if not keywords:
             return {}
         pinyin_highlights = {}
         for field in match_fields:
             if field.endswith(".pinyin"):
                 no_pinyin_field = field.replace(".pinyin", "")
-                text = get_es_source_val(_source, no_pinyin_field)
+                text = get_es_source_val(source, no_pinyin_field)
                 if text is None:
                     continue
                 highlighted_text = self.pinyin_highlighter.highlight(
-                    query, text, tag="hit"
+                    keywords, text, tag="hit"
                 )
                 if highlighted_text:
                     pinyin_highlights[field] = [highlighted_text]
         return pinyin_highlights
+
+    def get_merged_fields(
+        self, common_highlights: dict, pinyin_highlights: dict
+    ) -> set:
+        pinyin_highlight_fields = list(pinyin_highlights.keys())
+        common_highlight_fields = list(common_highlights.keys())
+        all_fields = set(pinyin_highlight_fields + common_highlight_fields)
+        pinyin_suffix_fields = [
+            field.replace(".pinyin", "") for field in pinyin_highlight_fields
+        ]
+        words_suffix_fields = [
+            field.replace(".words", "") for field in common_highlight_fields
+        ]
+        other_fields = [
+            field
+            for field in all_fields
+            if not any(field.endswith(suffix) for suffix in [".pinyin", ".words"])
+        ]
+        merged_fields = set(other_fields + pinyin_suffix_fields + words_suffix_fields)
+        return merged_fields
+
+    def get_merged_and_segged_highlights(
+        self,
+        source: dict,
+        common_highlights: dict,
+        pinyin_highlights: dict,
+    ) -> tuple[dict, dict]:
+        merged_highlights = {}
+        segged_highlights = {}
+        merged_fields = self.get_merged_fields(common_highlights, pinyin_highlights)
+        for field in merged_fields:
+            common_highlight = common_highlights.get(field, [])
+            words_highlight = common_highlights.get(field + ".words", [])
+            pinyin_highlight = pinyin_highlights.get(field + ".pinyin", [])
+            highlight_to_merge = common_highlight + words_highlight + pinyin_highlight
+            merged_res = self.highlight_merger.extract_and_merge(
+                get_es_source_val(source, field), highlight_to_merge, tag="hit"
+            )
+            merged_highlight = merged_res["merged"]
+            segged_highlight = merged_res["segged"]
+            if merged_highlight:
+                merged_highlights[field] = [merged_highlight]
+            if segged_highlight:
+                segged_highlights[field] = segged_highlight
+        return merged_highlights, segged_highlights
 
     def parse(
         self,
@@ -49,9 +98,8 @@ class VideoHitsParser:
         limit: int = -1,
         verbose: bool = False,
     ) -> dict:
-        query_info = self.split_query(query)
+        query_info = self.split_query_to_keywords_and_filters(query)
         qwords = query_info["keywords_body"]
-        qwords_str = " ".join(qwords)
         if not res_dict:
             hits_info = {
                 "query": query,
@@ -68,60 +116,30 @@ class VideoHitsParser:
             return hits_info
         hits = []
         for hit in res_dict["hits"]["hits"]:
-            _source = hit["_source"]
+            source = hit["_source"]
             score = hit["_score"]
             common_highlights = hit.get("highlight", {})
             pinyin_highlights = self.get_pinyin_highlights(
-                qwords_str, match_fields, _source
+                keywords=qwords, match_fields=match_fields, source=source
             )
-            all_fields = {**common_highlights, **pinyin_highlights}.keys()
-
-            merger = HighlightMerger()
-            segged_highlights = {}
-            merged_highlights = {}
-
-            pinyin_suffix_fields = [
-                field.replace(".pinyin", "") for field in pinyin_highlights.keys()
-            ]
-            words_suffix_fields = [
-                field.replace(".words", "") for field in common_highlights.keys()
-            ]
-            other_fields = [
-                field
-                for field in all_fields
-                if not any(field.endswith(suffix) for suffix in [".pinyin", ".words"])
-            ]
-            merged_fields = set(
-                other_fields + pinyin_suffix_fields + words_suffix_fields
+            merged_highlights, segged_highlights = (
+                self.get_merged_and_segged_highlights(
+                    source=source,
+                    common_highlights=common_highlights,
+                    pinyin_highlights=pinyin_highlights,
+                )
             )
-
-            for field in merged_fields:
-                common_highlight = common_highlights.get(field, [])
-                words_highlight = common_highlights.get(field + ".words", [])
-                pinyin_highlight = pinyin_highlights.get(field + ".pinyin", [])
-                highlight_to_merge = (
-                    common_highlight + words_highlight + pinyin_highlight
-                )
-                merged_res = merger.extract_and_merge(
-                    get_es_source_val(_source, field), highlight_to_merge, tag="hit"
-                )
-                merged_highlight = merged_res["merged"]
-                segged_highlight = merged_res["segged"]
-                if merged_highlight:
-                    merged_highlights[field] = [merged_highlight]
-                if segged_highlight:
-                    segged_highlights[field] = segged_highlight
-
-            if (
+            is_hit_ignored = (
                 drop_no_highlights
-                and (not common_highlights)
-                and (not pinyin_highlights)
                 and match_type == "most_fields"
                 and match_operator == "or"
-            ):
+                and (not common_highlights)
+                and (not pinyin_highlights)
+            )
+            if is_hit_ignored:
                 continue
             hit_info = {
-                **_source,
+                **source,
                 "score": score,
                 "highlights": {
                     "common": common_highlights,
@@ -150,13 +168,15 @@ class VideoHitsParser:
             },
             "query_info": query_info,
         }
-        logger.enter_quiet(not verbose)
-        # logger.success(pformat(hits_info, indent=4, sort_dicts=False))
-        logger.note(f"Request type: [{request_type}]")
-        logger.mesg(f"  * detail level: {detail_level}")
-        logger.mesg(f"  * return hits count: {hits_info['return_hits']}")
-        logger.mesg(f"  * total hits count: {hits_info['total_hits']}")
-        logger.mesg(f"  * took: {hits_info['took']}ms")
-        logger.mesg(f"  * timed_out: {hits_info['timed_out']}")
-        logger.exit_quiet(not verbose)
+        if verbose:
+            # logger.success(pformat(hits_info, indent=4, sort_dicts=False))
+            logger.note(f"Request type: [{request_type}]")
+            logger.mesg(f"  * detail level: {detail_level}")
+            logger.mesg(f"  * return hits count: {hits_info['return_hits']}")
+            logger.mesg(f"  * total hits count: {hits_info['total_hits']}")
+            logger.mesg(f"  * took: {hits_info['took']}ms")
+            logger.mesg(f"  * timed_out: {hits_info['timed_out']}")
         return hits_info
+
+
+VideoHitsParser = VideoHitsParserV1
