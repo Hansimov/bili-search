e98541c0967e48fe7660f2e9f9346f61b9ddad32
Hansimov <591172499@qq.com>
2025-03-16 07:30:42 +0800

    :zap: [Enhance] WordExprElasticConverter: add switch_mode to modify fields and types dynamically, and moduralize node_to_match_dict

--- converters/dsl/fields/word.py
+++ converters/dsl/fields/word.py
@@ -1,5 +1,7 @@
+from typing import Literal, Union
+
 from converters.dsl.node import DslExprNode
-from converters.dsl.constants import WORD_OPS
+from converters.dsl.constants import WORD_OPS, TEXT_TYPES
 from elastics.videos.constants import SEARCH_MATCH_FIELDS, SEARCH_BOOSTED_FIELDS
 from elastics.videos.constants import SUGGEST_MATCH_FIELDS, SUGGEST_BOOSTED_FIELDS
 from elastics.videos.constants import DATE_MATCH_FIELDS, DATE_BOOSTED_FIELDS
@@ -9,40 +11,62 @@ from elastics.videos.constants import SEARCH_MATCH_TYPE, SUGGEST_MATCH_TYPE
 
 
 class WordExprElasticConverter:
-    def __init__(
+    def __init__(self, mode: Literal["search", "suggest"] = "search"):
+        self.mode = mode
+        self.switch_mode(mode)
+
+    def switch_mode(
         self,
-        match_fields: list[str] = SEARCH_MATCH_FIELDS,
-        match_type: MATCH_TYPE = SEARCH_MATCH_TYPE,
-        query_type: QUERY_TYPE = QUERY_TYPE_DEFAULT,
+        mode: Literal["search", "suggest"] = None,
+        match_fields: list[str] = None,
+        date_match_fields: list[str] = None,
+        match_type: MATCH_TYPE = None,
+        query_type: QUERY_TYPE = None,
     ):
-        self.match_fields = match_fields
-        self.match_type = match_type
-        self.query_type = query_type
+        if mode == "suggest":
+            self.match_fields = match_fields or SUGGEST_MATCH_FIELDS
+        else:
+            self.match_fields = match_fields or SEARCH_MATCH_FIELDS
+        self.date_match_fields = date_match_fields or DATE_MATCH_FIELDS
+        self.match_type = match_type or SEARCH_MATCH_TYPE
+        self.query_type = query_type or QUERY_TYPE_DEFAULT
 
     def clean_word_val(self, val: str) -> str:
         return val.strip('" ')
 
-    def query_to_match_dict(self, query: str) -> dict:
+    def query_to_match_dict(self, query: str, is_date_format: bool = False) -> dict:
+        if is_date_format:
+            match_fields = self.date_match_fields
+        else:
+            match_fields = self.match_fields
         return {
             self.query_type: {
                 "query": query,
                 "type": self.match_type,
-                "fields": self.match_fields,
+                "fields": match_fields,
             }
         }
 
-    def convert_multi(self, nodes: list[DslExprNode]) -> dict:
-        texts = []
-        for node in nodes:
-            text_node = node.find_child_with_key(["text_quoted", "text_strict"])
-            text = text_node.get_deepest_node_value()
-            if text:
-                text = self.clean_word_val(text)
-                texts.append(text)
-        if len(texts) == 1:
-            return self.query_to_match_dict(texts[0])
+    def node_to_match_dict(self, node: DslExprNode) -> dict:
+        text_node = node.find_child_with_key(TEXT_TYPES)
+        text = text_node.get_deepest_node_value()
+        is_date_format = node.extras.get("is_date_format", False)
+        if text:
+            text = self.clean_word_val(text)
+            return self.query_to_match_dict(text, is_date_format=is_date_format)
         else:
-            return [self.query_to_match_dict(text) for text in texts]
+            return {}
+
+    def convert_multi(self, nodes: list[DslExprNode]) -> Union[dict, list[dict]]:
+        node_dicts = []
+        for node in nodes:
+            node_dict = self.node_to_match_dict(node)
+            if node_dict:
+                node_dicts.append(node_dict)
+        if len(node_dicts) == 1:
+            return node_dicts[0]
+        else:
+            return node_dicts
 
     def convert(self, node: DslExprNode) -> dict:
         """node key is `word_expr`"""
