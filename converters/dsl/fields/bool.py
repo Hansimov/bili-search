47c185dfd0147e2bfd069ea27aaebe99dd8f9209
Hansimov <591172499@qq.com>
2025-03-20 02:30:08 +0800

    :zap: [Enhance] BoolElasticReducer: add multi_match case in get_bool_ops_and_dicts, and improve reduce_co_bool_clauses with should-related parts

--- converters/dsl/fields/bool.py
+++ converters/dsl/fields/bool.py
@@ -42,20 +42,61 @@ class BoolElasticReducer:
     def get_bool_ops_and_dicts(
         self, bool_clause: dict
     ) -> list[tuple[ES_BOOL_OP_TYPE, dict]]:
-        """Get list of bool ops and dicts of bool_clause"""
+        """Return list of tuples: [(bool_op, bool_dict), ...]"""
         bool_ops_and_dicts = []
+        # case: {"bool": {"must": ...}}
         bool_items = bool_clause.get("bool", {})
         for bool_op, bool_dict in bool_items.items():
-            if bool_dict:
+            if bool_dict is not None:
                 bool_ops_and_dicts.append((bool_op, bool_dict))
+        # case: {"filter": ...}
         filter_dict = bool_clause.get("filter", {})
         if filter_dict:
             bool_ops_and_dicts.append(("filter", filter_dict))
+        # case: {"multi_match": {"query": ...}}
+        if not bool_items and not filter_dict:
+            bool_ops_and_dicts.append(("must", bool_clause))
         return bool_ops_and_dicts
 
     def get_minimum_should_match(self, bool_clause: dict) -> int:
         if bool_clause.get("bool", {}).get("should"):
             return bool_clause["bool"].get(MSM, None)
+
+    def combine_zero_msm_clauses(self, should_clauses: list[dict]) -> list[dict]:
+        """Input: list of: {"should": ..., "minimum_should_match": 0}
+        Output: {"should": [...]}"""
+        return [should_clause["should"] for should_clause in should_clauses]
+
+    def combine_non_zero_msm_clauses(self, should_clauses: list[dict]) -> list[dict]:
+        """Input: list of: {"should": ..., "minimum_should_match": X}
+        Output: {"should": [...]}"""
+        return [
+            {"bool": {"should": should_clause["should"], MSM: should_clause[MSM]}}
+            for should_clause in should_clauses
+        ]
+
+    def add_shoulds_to_op_list_dict(
+        self, op_list_dict: dict, should_clauses: list[dict]
+    ) -> dict:
+        if len(should_clauses) == 0:
+            return op_list_dict
+        if len(should_clauses) == 1:
+            op_list_dict.update(should_clauses[0])
+            return op_list_dict
+        zero_msm_clauses = []
+        non_zero_msm_clauses = []
+        for should_clause in should_clauses:
+            if should_clause.get(MSM, None) == 0:
+                zero_msm_clauses.append(should_clause)
+            else:
+                non_zero_msm_clauses.append(should_clause)
+        if zero_msm_clauses:
+            op_list_dict["should"] = self.combine_zero_msm_clauses(zero_msm_clauses)
+            op_list_dict[MSM] = 0
+        op_list_dict["must"].extend(
+            self.combine_non_zero_msm_clauses(non_zero_msm_clauses)
+        )
+        return op_list_dict
 
     def sort_op_list_dict(self, op_list_dict: dict) -> dict:
         """Sort op_list_dict keys by:
@@ -71,26 +112,34 @@ class BoolElasticReducer:
         )
         return op_list_dict
 
-    def reduce_bool_clauses(self, bool_clauses: list[dict], sort: bool = True) -> dict:
-        """Reduce list of bool clauses to single bool clause"""
+    def reduce_co_bool_clauses(
+        self, bool_clauses: list[dict], sort: bool = True
+    ) -> dict:
+        """Return a single bool clause dict, which is reduced from list of bool clauses (under `co` or `and`)"""
         if not bool_clauses:
             return {}
         if len(bool_clauses) == 1:
             return bool_clauses[0]
         op_list_dict = defaultdict(list)
+        should_clauses = []
         for bool_clause in bool_clauses:
             bool_ops_and_dict = self.get_bool_ops_and_dicts(bool_clause)
-            if bool_ops_and_dict:
-                for bool_op, bool_dict in bool_ops_and_dict:
+            if not bool_ops_and_dict:
+                continue
+            for bool_op, bool_dict in bool_ops_and_dict:
+                if bool_op == "should":
+                    should_clause = {
+                        "should": bool_dict,
+                        MSM: self.get_minimum_should_match(bool_clause),
+                    }
+                    should_clauses.append(should_clause)
+                elif bool_op == MSM:
+                    continue
+                else:
                     op_list_dict[bool_op].append(bool_dict)
-                    if bool_op == "should":
-                        min_should = self.get_minimum_should_match(bool_clause)
-                        if min_should is not None:
-                            op_list_dict[MSM].append(min_should)
+        op_list_dict = self.add_shoulds_to_op_list_dict(op_list_dict, should_clauses)
         for bool_op, bool_dict_list in op_list_dict.items():
-            if bool_op == MSM:
-                op_list_dict[MSM] = min(bool_dict_list)
-            if len(bool_dict_list) == 1:
+            if isinstance(bool_dict_list, list) and len(bool_dict_list) == 1:
                 op_list_dict[bool_op] = bool_dict_list[0]
         if sort:
             op_list_dict = self.sort_op_list_dict(op_list_dict)
