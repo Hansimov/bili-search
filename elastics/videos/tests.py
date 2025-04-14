ad81270b47c893f9e0700098b0eaf1dab86eac2f
Hansimov <591172499@qq.com>
2025-04-14 19:10:59 +0800

    :zap: [Enhance] elastics.videos.tests: new test_agg and test_explore

--- elastics/videos/tests.py
+++ elastics/videos/tests.py
@@ -1,107 +1,20 @@
 from tclogger import logger, logstr, dict_to_str, brk
 
-from elastics.videos.searcher import VideoSearcher
+from elastics.videos.searcher import VideoSearcherV2
+from elastics.videos.explorer import VideoExplorer
 from elastics.videos.constants import VIDEOS_INDEX_DEFAULT
 from converters.query.dsl import MultiMatchQueryDSLConstructor
 from converters.query.dsl import ScriptScoreQueryDSLConstructor
 from converters.query.filter import QueryFilterExtractor
 
-searcher = VideoSearcher(VIDEOS_INDEX_DEFAULT)
+searcher = VideoSearcherV2(VIDEOS_INDEX_DEFAULT)
+explorer = VideoExplorer(VIDEOS_INDEX_DEFAULT)
 
 
 def test_random():
     logger.note("> Getting random results ...")
     res = searcher.random(limit=3)
     logger.mesg(dict_to_str(res))
-
-
-suggest_queries = [
-    # "影视飓feng",
-    # "yingshiju",
-    # "yingshi ju",
-    # "影视ju",
-    # "hongjing 08",
-    # "Hongjing 08? 2024",
-    # "Hongjing (08 || 月亮3) 2024",
-    # "hongjing 小快地? (08 || 月亮3) d=15d u=红警最top",
-    # "hongjing (xiaokuaidi || bingtian)"
-    # "俞利均",
-    # "liangwenfeng",
-    # "xiaomi",
-    # "d=3d",
-    "u=雷军",
-    # "Hongjing 08 xiaokuaidi 2024",
-    # "5197",
-    # "影视 ju :date<=7d 2024",
-    # "edmundd",
-    # "gpt-sovits huaer",
-    # "XIAOMI su7",
-    # "Niko",
-    # "shanghai major hanlengde",
-    # "after effects jiaocheng",
-    # "damoxing weidiao",
-    # "大模型 微调",
-    # "d=1d",
-    # "llm 大模型",
-    # "are you ok",
-    # "changcheng",
-    # "10-22",
-    # "Python 教程",
-    # ":date=2024-10-31 :view>=1w",
-]
-
-
-def test_suggest():
-    for query in suggest_queries:
-        query_str = logstr.mesg(brk(query))
-        logger.note(f"> Query: {query_str}")
-        res = searcher.multi_level_suggest(query, limit=20, verbose=True)
-        hits = res.pop("hits")
-        # for idx, hit in enumerate(hits[:3]):
-        #     logger.note(f"* Hit {idx}:")
-        #     logger.file(dict_to_str(hit, align_list=False), indent=4)
-        logger.success(f"✓ Suggest results:")
-        logger.success(dict_to_str(res, align_list=False), indent=2)
-
-
-multi_level_search_queries = [
-    "are you ok",
-    "游戏星",
-    "10-22",
-]
-
-
-def test_multi_level_search():
-    for query in multi_level_search_queries:
-        logger.note("> Searching results:", end=" ")
-        logger.file(f"[{query}]")
-        res = searcher.multi_level_search(
-            query, limit=500, use_script_score=True, verbose=True
-        )
-        hits = res.pop("hits")
-        logger.success(dict_to_str(res))
-        # for idx, hit in enumerate(hits[:3]):
-        #     logger.note(f"* Hit {idx}:")
-        #     logger.file(dict_to_str(hit, align_list=False), indent=4)
-
-
-search_queries = [
-    "影视飓风",
-]
-
-
-def test_search():
-    for query in search_queries:
-        logger.note("> Searching results:", end=" ")
-        logger.file(f"[{query}]")
-        res = searcher.search(
-            query, limit=50, detail_level=3, use_script_score=True, verbose=True
-        )
-        hits = res.pop("hits")
-        logger.success(dict_to_str(res))
-        # for idx, hit in enumerate(hits):
-        #     logger.note(f"* Hit {idx}:")
-        #     logger.file(dict_to_str(hit, align_list=False), indent=4)
 
 
 filter_queries = [
@@ -155,11 +68,148 @@ def test_filter():
             logger.warn(dict_to_str(search_res))
 
 
+suggest_queries = [
+    # "影视飓feng",
+    # "yingshiju",
+    # "yingshi ju",
+    # "影视ju",
+    # "hongjing 08",
+    # "Hongjing 08? 2024",
+    # "Hongjing (08 || 月亮3) 2024",
+    # "hongjing 小快地? (08 || 月亮3) d=15d u=红警最top",
+    # "hongjing (xiaokuaidi || bingtian)"
+    # "俞利均",
+    # "liangwenfeng",
+    "deepseek v3 0324",
+    # "xiaomi",
+    # "d=3d",
+    # "u=雷军",
+    # "d=2d v>1w",
+    # "v=[1k,10000] d=2d",
+    # "d=2d v=[1k,10000]",
+    # "v=[1k,10000]",
+    # "bv=BV1LP4y1H7TJ",
+    # "d=[1d,3d]",
+    # "bv=[BV1Pt411o7Zb,BV1Xx411c7cH]",
+    # "av=[1,2,4]",
+    # "bv=[BV1Pt411o7Zb,2]",
+    # "Hongjing 08 xiaokuaidi 2024",
+    # "5197",
+    # "影视 ju :date<=7d 2024",
+    # ":date<=7d 2024",
+    # "edmundd",
+    # "gpt-sovits huaer",
+    # "XIAOMI su7",
+    # "Niko",
+    # "shanghai major hanlengde",
+    # "after effects jiaocheng",
+    # "damoxing weidiao",
+    # "大模型 微调",
+    # "d=1d",
+    # "llm 大模型",
+    # "are you ok",
+    # "changcheng",
+    # "10-22",
+    # "Python 教程",
+    # ":date=2024-10-31 :view>=1w",
+]
+
+
+def test_suggest():
+    for query in suggest_queries:
+        query_str = logstr.mesg(brk(query))
+        logger.note(f"> Query: {query_str}")
+        res = searcher.multi_level_suggest(query, limit=20, verbose=True)
+        hits = res.pop("hits")
+        # for idx, hit in enumerate(hits[:3]):
+        #     logger.note(f"* Hit {idx}:")
+        #     logger.file(dict_to_str(hit, align_list=False), indent=4)
+        logger.success(f"✓ Suggest results:")
+        logger.success(dict_to_str(res, align_list=False), indent=2)
+
+
+multi_level_search_queries = [
+    "are you ok",
+    "游戏星",
+    "10-22",
+]
+
+
+def test_multi_level_search():
+    for query in multi_level_search_queries:
+        logger.note("> Searching results:", end=" ")
+        logger.file(f"[{query}]")
+        res = searcher.multi_level_search(
+            query, limit=500, use_script_score=True, verbose=True
+        )
+        hits = res.pop("hits")
+        logger.success(dict_to_str(res))
+        # for idx, hit in enumerate(hits[:3]):
+        #     logger.note(f"* Hit {idx}:")
+        #     logger.file(dict_to_str(hit, align_list=False), indent=4)
+
+
+search_queries = [
+    # "影视飓风^3 巴黎",
+    # "雷军 AI (锐评 || 吐槽^3 || 游戏^4)",
+    # "deepseek v3 2024",
+    # "deepseek v3 0324 d=1d",
+    # "day by day",
+    # "v>1k 原神",
+    # "v>1 k=原神",
+    # '+"影视飓风" +“李四维”',
+    # '+"影视飓风" +“-lks-”',
+    # '《"你的名字"》',
+    # '"何同学" v>1k',
+    # '"何同学"',
+    '"影视飓风"'
+]
+
+
+def test_search():
+    for query in search_queries:
+        logger.note("> Searching results:", end=" ")
+        logger.file(f"[{query}]")
+        res = searcher.search(
+            query,
+            limit=50,
+            detail_level=1,
+            use_script_score=True,
+            verbose=True,
+        )
+        # hits = res.pop("hits")
+        # logger.success(dict_to_str(res))
+        # for idx, hit in enumerate(hits[:3]):
+        #     logger.note(f"* Hit {idx}:")
+        #     logger.file(dict_to_str(hit, align_list=False), indent=4)
+
+
+def test_agg():
+    for query in search_queries:
+        logger.note("> Agg results:", end=" ")
+        logger.file(f"[{query}]")
+        res = searcher.agg(query, verbose=False)
+        logger.success(dict_to_str(res, align_list=False), indent=2)
+
+
+def test_explore():
+    for query in search_queries:
+        logger.note("> Explore results:", end=" ")
+        logger.file(f"[{query}]")
+        explore_res = explorer.explore(query, verbose=True)
+        for step_res in explore_res:
+            stage_name = step_res["name"]
+            logger.hint(f"* stage result of {(logstr.mesg(brk(stage_name)))}:")
+            logger.mesg(dict_to_str(step_res, align_list=False), indent=2)
+
+
 if __name__ == "__main__":
     # test_random()
-    test_suggest()
+    # test_filter()
+    # test_suggest()
     # test_multi_level_search()
     # test_search()
-    # test_filter()
+    # test_agg()
+    test_explore()
 
     # python -m elastics.videos.tests
